a26a4af9f3130bac181cd97562dcc72bc2841e12
JTan2231 <j.tan2231@gmail.com>
2021-04-19 06:40:15 -0400

    Cleaning

--- jobscrape.py
+++ jobscrape.py
@@ -12,23 +12,25 @@ PUNCTUATION = "><,.:;'\"[{}]|\\=_()*&^%$@!"
 MAX_WORKERS = 100
 MAX_PAGE_LIMIT = 1000
 
-def increment_dict(d, word):
-    if not word in d:
-        d[word] = 1
-    else:
-        d[word] += 1
-
-    return d
-
-def sort_dict(d):
-    return { k: v for k, v in sorted(d.items(), key=lambda item: item[1]) }
-
 class Parser(HTMLParser):
     frequencies = dict()
 
     getting_keys = True
 
     keys = []
+
+    def load_url(self, url):
+        with urllib.request.urlopen(url) as url:
+            return url.read().decode()
+
+    def sort_dict(self):
+        self.frequencies = { k: v for k, v in sorted(self.frequencies.items(), key=lambda item: item[1]) }
+
+    def increment_dict(self, word):
+        if not word in self.frequencies:
+            self.frequencies[word] = 1
+        else:
+            self.frequencies[word] += 1
 
     def handle_starttag(self, tag, attrs):
         if self.getting_keys:
@@ -51,101 +53,103 @@ class Parser(HTMLParser):
                 if len(word) == 0 or not word in whitelist:
                     continue
 
-                self.frequencies = increment_dict(self.frequencies, word)
+                self.increment_dict(word)
 
-def load_url(url):
-    with urllib.request.urlopen(url) as url:
-        return url.read().decode()
+    def get_keys(self, locations, page_limit=None):
+        page_limit = 999 if None else page_limit
 
-def get_keys(parser, locations, page_limit=None):
-    page_limit = 999 if None else page_limit
+        for location in locations:
+            location = location.replace(' ', '%20')
+            base_link = "https://www.engineerjobs.com/jobs/search?from=homepage_searchbox&ak=software+engineer&l="+location+"&page="
+            links = [base_link+str(i+1) for i in range(page_limit)]
 
-    for location in locations:
-        location = location.replace(' ', '%20')
-        base_link = "https://www.engineerjobs.com/jobs/search?from=homepage_searchbox&ak=software+engineer&l="+location+"&page="
-        links = [base_link+str(i+1) for i in range(page_limit)]
+            self.getting_keys = True
+            with concurrent.futures.ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
+                future_to_url = (executor.submit(self.load_url, url) for url in links)
+                for future in tqdm(concurrent.futures.as_completed(future_to_url)):
+                    html_string = future.result()
+                    self.feed(html_string)
 
-        parser.getting_keys = True
+        a = "https://www.engineerjobs.com/api/job?key="
+        b = "&isp=0&al=1&ia=0&tk=1f3is9ubjt4at800&tkt=serp"
+        self.keys = [a+key+b for key in self.keys]
+
+    def build_frequencies(self):
+        self.getting_keys = False
         with concurrent.futures.ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
-            future_to_url = (executor.submit(load_url, url) for url in links)
+            future_to_url = (executor.submit(self.load_url, url) for url in self.keys)
             for future in tqdm(concurrent.futures.as_completed(future_to_url)):
-                html_string = future.result()
-                parser.feed(html_string)
+                try:
+                    data = json.loads(future.result())
+                    description = data['job']['description']
+                    self.feed(description)
 
-    a = "https://www.engineerjobs.com/api/job?key="
-    b = "&isp=0&al=1&ia=0&tk=1f3is9ubjt4at800&tkt=serp"
-    parser.keys = [a+key+b for key in parser.keys]
+                    for skill in data['skillEntities']:
+                        word = skill.lower()
+                        self.increment_dict(word)
 
-    return parser
+                except urllib.error.HTTPError as e:
+                    print("Error!", e)
+                    continue
 
-def build_parser(parser):
-    with concurrent.futures.ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
-        future_to_url = (executor.submit(load_url, url) for url in parser.keys)
-        for future in tqdm(concurrent.futures.as_completed(future_to_url)):
-            try:
-                data = json.loads(future.result())
-                description = data['job']['description']
-                parser.feed(description)
+if __name__ == "__main__":
+    parser = Parser()
+    locations = []
+    locations_default = ["Seattle, WA", "Columbus, OH", "Los Angeles, CA", "San Diego, CA",
+                         "New York City, NY", "Austin, TX", "Dallas, TX", "Portland, OR"]
 
-                for skill in data['skillEntities']:
-                    word = skill.lower()
-                    parser.frequencies = increment_dict(parser.frequencies, word)
+    input_string = "Input location as City, ST (empty input to continue): "
 
-            except urllib.error.HTTPError as e:
-                print("Error!", e)
-                continue
+    print("Default locations: ")
+    for location in locations_default:
+        print(location)
+    print(' ')
 
-    return parser
-
-parser = Parser()
-locations = []
-locations_default = ["Seattle, WA", "Columbus, OH", "Los Angeles, CA", "San Diego, CA", "New York City, NY", "Austin, TX", "Dallas, TX"]
-input_string = "Input location as City, ST (empty input to continue): "
-print("Default input: ")
-for location in locations_default:
-    print(location)
-print(' ')
-
-location = input(input_string)
-while len(location) > 0:
-    locations.append(location)
     location = input(input_string)
+    while len(location) > 0:
+        locations.append(location)
+        location = input(input_string)
 
-if len(locations) == 0:
-    locations = locations_default
+    if len(locations) == 0:
+        locations = locations_default
 
-correct_input = False
-page_limit = 10
-while not correct_input:
-    try:
-        inp = input("Input number of pages to scrape (max 1000, default 10): ")
-        if len(inp) == 0:
-            break
+    correct_input = False
+    page_limit = 10
+    while not correct_input:
+        try:
+            inp = input("Number of pages to scrape (max=1000, default=10): ")
+            if len(inp) == 0:
+                break
 
-        page_limit = int(inp)
-        page_limit = int(page_limit)
-        correct_input = True
-    except ValueError:
-        print("Number must be decimal. e.g. 10")
+            page_limit = int(inp)
+            page_limit = int(page_limit)
+            correct_input = True
+        except ValueError:
+            print("Number must be decimal. e.g. 10")
 
-page_limit = min(page_limit, MAX_PAGE_LIMIT)
+    page_limit = min(page_limit, MAX_PAGE_LIMIT)
 
-print("Gathering job posting keys...")
-parser = get_keys(parser, locations, page_limit)
-print(f"{len(parser.keys)} keys gathered.")
-print('')
-print("Counting key word appearances...")
-parser = build_parser(parser)
+    output_filename = input("Output filename, no extension (default='scrape')")
+    if len(output_filename) == 0:
+        output_filename = "scrape"
 
-parser.frequencies = sort_dict(parser.frequencies)
+    output_filename += ".txt"
 
-with open("scrape.txt", 'w') as f:
-    f.write('') # clear file
+    print("Gathering job posting keys...")
+    parser.get_keys(locations, page_limit)
+    print(f"{len(parser.keys)} job postings gathered.\n")
+    print("Counting key word frequencies...")
+    parser.build_frequencies()
 
-with open("scrape.txt", 'a') as f:
-    for key, value in parser.frequencies.items():
-        s = f"{key}: {value}\n"
-        print(s[:-1])
-        f.write(s)
+    parser.sort_dict()
 
-print("Displaying word frequencies in console. Output saved to 'scrape.txt'")
+    with open("scrape.txt", 'w') as f:
+        f.write('') # clear file
+
+    with open("scrape.txt", 'a') as f:
+        for key, value in parser.frequencies.items():
+            s = f"{key}: {value}\n"
+            print(s[:-1])
+            f.write(s)
+
+    print(f"Output saved to '{output_filename}'")
