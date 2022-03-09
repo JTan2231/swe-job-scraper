e408541c404cf1d81f1b648b6aeb59bc87543fa7
Joey <j.tan2231@gmail.com>
2022-03-08 22:34:06 -0500

    more visualization

--- visualize.py
+++ visualize.py
@@ -12,9 +12,24 @@ try:
 except:
     print("Error: couldn't find `whitelist.csv`")
 
-fig, ax = pl.subplots(1, 1)
-ax.imshow(coocc)
-ax.set_xticks(list(range(len(whitelist))), labels=whitelist)
-ax.set_yticks(list(range(len(whitelist))), labels=whitelist)
-pl.setp(ax.get_xticklabels(), rotation=90, horizontalalignment='right')
-pl.show()
+try:
+    frequencies = pd.read_csv("frequencies.csv")
+    frequencies = frequencies.loc[frequencies['frequency'] > 100]
+except:
+    print("Error: couldn't find `whitelist.csv`")
+
+which = input("bar or matrix: ")
+
+if which == "matrix":
+    fig, ax = pl.subplots(1, 1)
+    ax.imshow(coocc)
+    ax.set_xticks(list(range(len(whitelist))), labels=whitelist)
+    ax.set_yticks(list(range(len(whitelist))), labels=whitelist)
+    pl.show()
+elif which == "bar":
+    fig, ax = pl.subplots(1, 1)
+    ax.bar(frequencies['word'], frequencies['frequency'])
+    pl.setp(ax.get_xticklabels(), rotation=90, horizontalalignment='right')
+    pl.show()
+else:
+    print("what?")
